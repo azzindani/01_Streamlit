4bea010cb5146f4d42ac415236207c0a34113a0c
Afza Azzindani <109005550+azzindani@users.noreply.github.com>
2024-11-12 14:20:44 +0700

    Update NYC_Property_Dashboard.py

--- NYC_Property_Dashboard.py
+++ NYC_Property_Dashboard.py
@@ -192,12 +192,11 @@ with col_13:
 
 chart_df_1 = df.groupby(by = category, as_index = False,)[['Sale Price']].sum()
 chart_df_1 = chart_df_1.rename(columns = {'Sale Price' : 'Sales (USD)'})
-chart_df_1 = chart_df_1.sort_values(by = 'Sales (USD)', ascending = True)
 
 with col_11:
   title = 'Sales Value by ' + category
   fig = px.bar(
-    chart_df_1,
+    chart_df_1.sort_values(by = 'Sales (USD)', ascending = True),
     y = category,
     x = 'Sales (USD)',
     title = title,
@@ -230,12 +229,11 @@ with col_11:
 
 chart_df_2 = df.groupby(by = category, as_index = False,)[['Sale Price']].count()
 chart_df_2 = chart_df_2.rename(columns = {'Sale Price' : 'Qty'})
-chart_df_2 = chart_df_2.sort_values(by = 'Qty', ascending = True)
 
 with col_12:
   title = 'Property Sold by ' + category
   fig = px.bar(
-    chart_df_2,
+    chart_df_2.sort_values(by = 'Qty', ascending = True),
     y = category,
     x = 'Qty',
     title = title,
@@ -304,12 +302,11 @@ filtered_df['Month & Year'] = filtered_df['Sale Date'].dt.to_period('M')
 chart_df_4 = pd.DataFrame(filtered_df.groupby([filtered_df['Month & Year'], category])[[price]].mean()).reset_index()
 chart_df_4['Month & Year'] = chart_df_4['Month & Year'].astype(str)
 chart_df_4 = chart_df_4.rename(columns = {price : 'Average ' + price + ' (USD)'})
-chart_df_4 = chart_df_4.sort_values(by = 'Average ' + price + ' (USD)', ascending = True)
 
 with col_22:
   title = 'Average ' + price + ' by ' + category
   fig = px.line(
-    chart_df_4,
+    chart_df_4.sort_values(by = 'Average ' + price + ' (USD)', ascending = True),
     x = 'Month & Year',
     y = 'Average ' + price + ' (USD)',
     color = category,
