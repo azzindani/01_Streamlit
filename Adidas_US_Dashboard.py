6252e13ef89d05d2b25dcb7bfce0f9726f2f239d
Afza Azzindani <109005550+azzindani@users.noreply.github.com>
2024-11-12 12:46:19 +0700

    Update Adidas_US_Dashboard.py

--- Adidas_US_Dashboard.py
+++ Adidas_US_Dashboard.py
@@ -195,7 +195,7 @@ with col_13:
 # create bar chart
 
 chart_df_1 = filtered_df.groupby(by = 'Retailer', as_index = False,)[['Total Sales']].sum()
-chart_df_1 = chart_df_1.sort_values(by = 'Total Sales', ascending = False)
+chart_df_1 = chart_df_1.sort_values(by = 'Total Sales', ascending = True)
 
 with col_11:
   title = 'Sales Value by Retailer'
@@ -232,7 +232,7 @@ with col_11:
     st.download_button('Download Data', data = csv, file_name = title + '.csv', mime = 'text/csv', help = 'Click here to download as CSV file')
 
 chart_df_2 = filtered_df.groupby(by = 'Sales Method', as_index = False,)[['Total Sales']].sum()
-chart_df_2 = chart_df_2.sort_values(by = 'Total Sales', ascending = False)
+chart_df_2 = chart_df_2.sort_values(by = 'Total Sales', ascending = True)
 
 with col_12:
   title = 'Sales Value by Sales Method'
@@ -303,7 +303,7 @@ with col_21:
     st.download_button('Download Data', data = csv, file_name = title + '.csv', mime = 'text/csv', help = 'Click here to download as CSV file')
 
 chart_df_4 = filtered_df.groupby(by = ['Product'], as_index = False,)[['Total Sales', 'Operating Profit']].sum()
-chart_df_4 = chart_df_4.sort_values(by = 'Total Sales', ascending = False)
+chart_df_4 = chart_df_4.sort_values(by = 'Total Sales', ascending = True)
 
 with col_22:
   title = 'Sales & Operating Profit by Product'
@@ -340,7 +340,7 @@ with col_22:
     st.download_button('Download Data', data = csv, file_name = title + '.csv', mime = 'text/csv', help = 'Click here to download as CSV file')
 
 chart_df_5 = filtered_df.groupby(by = ['Product', 'Sales Method'], as_index = False,)[['Operating Margin']].mean()
-chart_df_5 = chart_df_5.sort_values(by = 'Operating Margin', ascending = False)
+chart_df_5 = chart_df_5.sort_values(by = 'Operating Margin', ascending = True)
 
 with col_23:
   title = 'Average Operating Margin'
@@ -375,7 +375,7 @@ with col_23:
     st.download_button('Download Data', data = csv, file_name = title + '.csv', mime = 'text/csv', help = 'Click here to download as CSV file')
 
 chart_df_6 = filtered_df.groupby(by = 'Product', as_index = False,)[['Total Sales']].sum()
-chart_df_6 = chart_df_6.sort_values(by = 'Total Sales', ascending = False)
+chart_df_6 = chart_df_6.sort_values(by = 'Total Sales', ascending = True)
 
 col_31, col_32 = st.columns((1, 1))
 
